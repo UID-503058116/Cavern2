a1e4a35e84b3a29eeccadf2012178122b28a6a3f
aki99_ <37666214+UID-503058116@users.noreply.github.com>
2025-02-03 04:58:58 +0800

    Update BlockSlipperyIce.java
    错误原因分析：
    IHumidityTile 接口要求实现 getHumidity 方法，但原代码中方法名拼写错误（getHumdiity），导致编译器认为该方法未实现接口的抽象方法。
    
    由于方法名错误，@Override 注解触发了 "方法不会覆盖或实现超类型的方法" 的警告。

--- src/main/java/cavern/block/BlockSlipperyIce.java
+++ src/main/java/cavern/block/BlockSlipperyIce.java
@@ -20,54 +20,54 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Optional.Interface;
 import net.minecraftforge.fml.common.Optional.InterfaceList;
 
-@InterfaceList
-({
-	@Interface(iface = "defeatedcrow.hac.api.climate.IHeatTile", modid = HaCPlugin.LIB_MODID, striprefs = true),
-	@Interface(iface = "defeatedcrow.hac.api.climate.IHumidityTile", modid = HaCPlugin.LIB_MODID, striprefs = true)
+@InterfaceList({
+    @Interface(iface = "defeatedcrow.hac.api.climate.IHeatTile", modid = HaCPlugin.LIB_MODID, striprefs = true),
+    @Interface(iface = "defeatedcrow.hac.api.climate.IHumidityTile", modid = HaCPlugin.LIB_MODID, striprefs = true)
 })
 public class BlockSlipperyIce extends BlockPackedIce implements IHeatTile, IHumidityTile
 {
-	public BlockSlipperyIce()
-	{
-		super();
-		this.setUnlocalizedName("slipperyIce");
-		this.setHardness(0.5F);
-		this.setSoundType(SoundType.GLASS);
-		this.setCreativeTab(Cavern.TAB_CAVERN);
-	}
+    public BlockSlipperyIce()
+    {
+        super();
+        this.setUnlocalizedName("slipperyIce");
+        this.setHardness(0.5F);
+        this.setSoundType(SoundType.GLASS);
+        this.setCreativeTab(Cavern.TAB_CAVERN);
+    }
 
-	@Override
-	public void onEntityWalk(World world, BlockPos pos, Entity entity)
-	{
-		super.onEntityWalk(world, pos, entity);
+    @Override
+    public void onEntityWalk(World world, BlockPos pos, Entity entity)
+    {
+        super.onEntityWalk(world, pos, entity);
 
-		if (!world.isRemote && entity.ticksExisted % 20 == 0 && entity instanceof EntityPlayer)
-		{
-			PlayerHelper.grantAdvancement((EntityPlayer)entity, "slip_ice");
-		}
-	}
+        if (!world.isRemote && entity.ticksExisted % 20 == 0 && entity instanceof EntityPlayer)
+        {
+            PlayerHelper.grantAdvancement((EntityPlayer) entity, "slip_ice");
+        }
+    }
 
-	@Override
-	public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing face)
-	{
-		return world.isAirBlock(pos.up()) ? BlockFaceShape.UNDEFINED : BlockFaceShape.SOLID;
-	}
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return world.isAirBlock(pos.up()) ? BlockFaceShape.UNDEFINED : BlockFaceShape.SOLID;
+    }
 
-	@Override
-	public float getSlipperiness(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity)
-	{
-		return 1.05F;
-	}
+    @Override
+    public float getSlipperiness(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity)
+    {
+        return 1.05F;
+    }
 
-	@Override
-	public DCHeatTier getHeatTier(World world, BlockPos target, BlockPos pos)
-	{
-		return DCHeatTier.COLD;
-	}
+    @Override
+    public DCHeatTier getHeatTier(World world, BlockPos target, BlockPos pos)
+    {
+        return DCHeatTier.COLD;
+    }
 
-	@Override
-	public DCHumidity getHumdiity(World world, BlockPos target, BlockPos pos)
-	{
-		return DCHumidity.WET;
-	}
+    // 修正方法名拼写错误：getHumdiity -> getHumidity
+    @Override
+    public DCHumidity getHumidity(World world, BlockPos target, BlockPos pos)
+    {
+        return DCHumidity.WET;
+    }
 }
